03647864228affdb6cc24b925591149ec278d09b
pyxyzc <pyxyzc@gmail.com>
2025-09-14 17:44:03 +0800

    :sparkles: Use nvim-tree to replace mini.files as directory management

---
+++ lua/plugins/nvim-tree.lua
@@ -0,0 +1,77 @@
+return {
+    "kyazdani42/nvim-tree.lua",
+    cmd = { "NvimTreeToggle", "NvimTreeFindFile" }, -- 按需加载
+    init = function()
+        local status, nvim_tree = pcall(require, "nvim-tree")
+        if not status then
+            vim.notify("No plugin named nvim-tree")
+            return
+        end
+        -- 列表操作快捷键
+        nvim_tree.setup({
+            sort_by = "case_sensitive",
+            -- 显示git状态图标
+            git = {
+                enable = true,
+            },
+            -- project plugin需要这样设置
+            update_cwd = true,
+            update_focused_file = {
+                enable = true,
+                update_cwd = true,
+            },
+            -- 隐藏.文件和node_modules文件夹
+            filters = {
+                dotfiles = false,
+                custom = { "node_modules" },
+            },
+            view = {
+                -- 宽度
+                width = 30,
+                -- 也可以'right'
+                side = "left",
+                -- 不显示行数
+                number = false,
+                relativenumber = false,
+                -- 显示图标
+                signcolumn = "yes",
+            },
+            renderer = {
+                root_folder_label = false,
+                group_empty = true,
+                icons = {
+                    show = {
+                        git = true,
+                        file = true,
+                        folder = true,
+                        folder_arrow = true,
+                    },
+                    glyphs = {
+                        bookmark = " ",
+                        folder = {
+                            arrow_closed = "⏵",
+                            arrow_open = "⏷",
+                        },
+                        git = {
+                            unstaged = "✗",
+                            staged = "✓",
+                            unmerged = "⌥",
+                            renamed = "➜",
+                            untracked = "★",
+                            deleted = "⊖",
+                            ignored = "◌",
+                        },
+                    },
+                },
+            },
+            actions = {
+                open_file = {
+                    -- 首次打开大小适配
+                    resize_window = true,
+                    -- 打开文件时关闭
+                    quit_on_open = false,
+                },
+            },
+        })
+    end,
+}
