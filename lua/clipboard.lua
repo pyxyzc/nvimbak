ae2475e37ae1bdd83c8dfb87fec789b735bc177b
pyxyzc <pyxyzc@gmail.com>
2025-09-15 23:37:43 +0800

    [Comment] Add some comments in clipboard module

--- lua/clipboard.lua
+++ lua/clipboard.lua
@@ -6,6 +6,9 @@ function my_paste(reg)
 	end
 end
 
+-- 只需要开启复制功能，不需要粘贴功能
+-- 粘贴直接由 insert 模式下 Ctrl+V 实现
+
 -- 通过环境变量检查是否是本地环境
 if os.getenv("SSH_TTY") == nil then
 	-- 当前环境为本地环境，包括 wsl
@@ -20,6 +23,8 @@ else
 			["*"] = require("vim.ui.clipboard.osc52").copy("*"),
 		},
 		paste = {
+            -- 自定义 paste，规避 OSC 52 读取本地剪贴板的内容
+            -- 直接返回 "" 寄存器的内容
 			-- 小括号里面的内容可能是毫无意义的，但是保持原样可能看起来更好一点
 			["+"] = my_paste("+"),
 			["*"] = my_paste("*"),
